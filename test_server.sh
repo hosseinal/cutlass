d2e065b486c55c4c597ee0ded0b3b0d9d811d7eb
HosseinAlbakri <hosseinalbakri3@gmail.com>
2025-11-30 15:02:07 -0500

    added test_server.sh

---
+++ test_server.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+##################### SLURM (do not change) v  #####################
+#SBATCH --export=ALL
+#SBATCH --job-name="project"
+#SBATCH --nodes=1
+#SBATCH --output="project.%j.%N.out"
+#SBATCH -t 01:00:00
+##################### SLURM (do not change) ^  #####################
+
+# Above are SLURM directives for job scheduling on a cluster
+export SLURM_CONF=/etc/slurm/slurm.conf
+
+mkdir -p build
+cd build 
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCUTLASS_NVCC_ARCHS="80"  
