1450afe61289fc7f20812d0f7568c597879c656b
HosseinAlbakri <hosseinalbakri3@gmail.com>
2025-12-01 12:21:42 -0500

    added some new methods for the run server

--- run_server.sh
+++ run_server.sh
@@ -16,19 +16,46 @@ cd build/examples/cute/tutorial/
 
 echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_1 512 512 32" > "output.txt"
 ./cute_tutorial_sgemm_1 512 512 32 >> "output.txt"
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_2 512 512 32" >> "output.txt"
+./cute_tutorial_sgemm_2 512 512 32 >> "output.txt"
+
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_1 512 512 64" >> "output.txt"
+./cute_tutorial_sgemm_1 512 512 64 >> "output.txt"
 echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_2 512 512 64" >> "output.txt"
 ./cute_tutorial_sgemm_2 512 512 64 >> "output.txt"
+
 echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_1 512 512 128" >> "output.txt"
 ./cute_tutorial_sgemm_1 512 512 128 >> "output.txt"
-echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_2 1024 256 32" >> "output.txt"
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_2 512 512 128" >> "output.txt"
+./cute_tutorial_sgemm_2 512 512 128 >> "output.txt"
+
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_1 1024 256 32" >> "output.txt"
 ./cute_tutorial_sgemm_1 1024 256 32 >> "output.txt"
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_2 1024 256 32" >> "output.txt"
+./cute_tutorial_sgemm_2 1024 256 32 >> "output.txt"
+
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_1 1024 256 64" >> "output.txt"
+./cute_tutorial_sgemm_1 1024 256 64 >> "output.txt"
 echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_2 1024 256 64" >> "output.txt"
 ./cute_tutorial_sgemm_2 1024 256 64 >> "output.txt"
+
+
 echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_1 1024 256 128" >> "output.txt"
 ./cute_tutorial_sgemm_1 1024 256 128 >>  "output.txt"
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_2 1024 256 128" >> "output.txt"
+./cute_tutorial_sgemm_2 1024 256 128 >> "output.txt"
+
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_1 2048 256 32" >> "output.txt"
+./cute_tutorial_sgemm_1 2048 256 32 >> "output.txt"
 echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_2 2048 256 32" >> "output.txt"
 ./cute_tutorial_sgemm_2 2048 256 32 >> "output.txt"
+
 echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_1 2048 256 64" >> "output.txt"
 ./cute_tutorial_sgemm_1 2048 256 64 >> "output.txt"
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_2 2048 256 64" >> "output.txt"
+./cute_tutorial_sgemm_2 2048 256 64 >> "output.txt"
+
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_1 2048 256 128" >> "output.txt"
+./cute_tutorial_sgemm_1 2048 256 128 >> "output.txt"
 echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_2 2048 256 128" >> "output.txt"
 ./cute_tutorial_sgemm_2 2048 256 128 >> "output.txt"
