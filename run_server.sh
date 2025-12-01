e135bf46822841ef09927ce6f980f11ffb3ed58c
HosseinAlbakri <hosseinalbakri3@gmail.com>
2025-12-01 12:18:16 -0500

    update the run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -13,5 +13,22 @@ export SLURM_CONF=/etc/slurm/slurm.conf
 
 cd build/examples/cute/tutorial/
 
-./cute_tutorial_sgemm_1 512 512 512
-./cute_tutorial_sgemm_2 512 512 512
+
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_1 512 512 32" > "output.txt"
+./cute_tutorial_sgemm_1 512 512 32 >> "output.txt"
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_2 512 512 64" >> "output.txt"
+./cute_tutorial_sgemm_2 512 512 64 >> "output.txt"
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_1 512 512 128" >> "output.txt"
+./cute_tutorial_sgemm_1 512 512 128 >> "output.txt"
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_2 1024 256 32" >> "output.txt"
+./cute_tutorial_sgemm_1 1024 256 32 >> "output.txt"
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_2 1024 256 64" >> "output.txt"
+./cute_tutorial_sgemm_2 1024 256 64 >> "output.txt"
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_1 1024 256 128" >> "output.txt"
+./cute_tutorial_sgemm_1 1024 256 128 >>  "output.txt"
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_2 2048 256 32" >> "output.txt"
+./cute_tutorial_sgemm_2 2048 256 32 >> "output.txt"
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_1 2048 256 64" >> "output.txt"
+./cute_tutorial_sgemm_1 2048 256 64 >> "output.txt"
+echo "Running Cute tutorial SGEMM examples cute_tutorial_sgemm_2 2048 256 128" >> "output.txt"
+./cute_tutorial_sgemm_2 2048 256 128 >> "output.txt"
